e41b62b81ac0c71f51217ade0fc83cf06c42438d
Asim Habib <de.asimhabib@gmail.com>
2020-10-13 19:05:00 +0500

    Implemented linked List

---
+++ src/linkedlist/main.java
@@ -0,0 +1,20 @@
+
+package linkedlist;
+
+/**
+ *
+ * @author asim
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        
+        list.insert(347);
+        list.insert(305);
+        list.insert(828);
+        list.insert(2000);
+        list.show();
+        
+    }
+}
